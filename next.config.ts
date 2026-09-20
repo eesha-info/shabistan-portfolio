2f86a193bd79402148a17a477aee6467336f4303
MD Eesha <Eesha.Mohammad@missiong3.com>
2026-09-20 16:17:15 +0530

    added config changes

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  output: 'export',              // generates static HTML/CSS/JS
+  basePath: '/shabistan-portfolio',
+  assetPrefix: '/shabistan-portfolio/',
+  images: {
+    unoptimized: true,           // GitHub Pages can't run Next's image optimizer
+  },
+  trailingSlash: true,           // helps GitHub Pages route folders correctly
 };
 
 module.exports = nextConfig;
